a1ce1d9e73aff8a5b8f7f17006533a4a6846bf52
ncmochacity <nickichenbattle@gmail.com>
2016-02-18 11:54:54 -0600

    smooth scrolling fix

--- js/animate.js
+++ js/animate.js
@@ -13,31 +13,31 @@ $(document).ready(function(){
 			},200);
 			setTimeout(function(){
 				$(".my-name").addClass("scroll-down");
-			},600);
+			},300);
 			setTimeout(function(){
 				$(".my-info").addClass("scroll-down");
-			},700);
-		}
-		if(x>=120){
-			setTimeout(function(){
-				$(".job").addClass("scroll-down");
-			},300);
-		}
-		if(x>=200){
-			setTimeout(function(){
-				$(".location").addClass("scroll-down");
 			},400);
 		}
-		if(x>=400){
+		if(x>=300){
+			setTimeout(function(){
+				$(".job").addClass("scroll-down");
+			},500);
+		}
+		if(x>=500){
+			setTimeout(function(){
+				$(".location").addClass("scroll-down");
+			},600);
+		}
+		if(x>=600){
 			setTimeout(function(){
 				$(".work").addClass("scroll-down");
-			},500);
+			},700);
 
 		}
-		if(x>=580){
+		if(x>=650){
 			setTimeout(function(){
 				$(".interests").addClass("scroll-down");
-			},600);
+			},800);
 		}
 		
 	})
